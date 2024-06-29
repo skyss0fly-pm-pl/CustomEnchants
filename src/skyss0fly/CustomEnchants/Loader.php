5cbd188e2cc452f8dfbe83bd01fe10417728df50
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-06-29 16:55:07 +1000

    Create Loader.php

---
+++ src/skyss0fly/CustomEnchants/Loader.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CustomEnchants\Loader;
+
+use CustomEnchants\TranslateTable;
+use CustomEnchants\Enchants\Lifesteal;
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase {
+public function registerEnchants() {
+  
+}
+
+}
