a3893cacdfa205e31f235a329d3f4b26f67a181c
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-06-29 16:27:07 +1000

    Create EnchantData.php

---
+++ src/skyss0fly/CustomEnchants/Enchants/EnchantData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CustomEnchants\EnchantData;
+
+
+interface EnchantData {
+/* Hey Devs, we have a format here:
+public const ENCHANTNAME = ["MaxLevel", "Tool/Armour/Weapon"];
+
+  */
+  
+  public const LIFESTEAL = ["5", "Weapon"];
+  public const BLINDING = ["3", "Weapon"];
+
+  
+
+}
